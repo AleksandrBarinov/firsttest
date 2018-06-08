90edd2c4d3835746e259b851c3cc542234ce346b
aleksandrbarinov <nosprut@gmail.com>
2018-06-08 12:23:58 +0300

    Initial commit

---
+++ src/main/java/InsuranceTestSber.java
@@ -0,0 +1,117 @@
+import org.junit.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.Assert.assertEquals;
+
+public class InsuranceTestSber {
+    private WebDriver driver;
+    private String baseUrl;
+
+    @Before
+    public void beforeTest (){
+        System.setProperty("webdriver.chrome.driver", "drv/chromedriver");
+        driver = new ChromeDriver();
+        baseUrl = "http://www.sberbank.ru/ru/person";
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+        driver.get(baseUrl);
+    }
+
+    @Test
+    public void testInsuranceSb (){
+
+        Wait<WebDriver> wait = new WebDriverWait(driver,15,1000);
+
+        WebElement insurance = driver.findElement(By.xpath("//span[text()='Страхование']"));
+        wait.until(ExpectedConditions.visibilityOf(insurance)).click();
+
+        driver.findElement
+                (By.xpath("//*[contains(@class,'lg-menu__sub-item')]//*[contains(text(),'Путешествия и покупки')]")).click();
+
+
+        WebElement registerOnline = driver.findElement
+                (By.xpath("//div[@data-pid='SBRF-TEXT-2247407']/..//a[text()='Оформить онлайн']"));
+        wait.until(ExpectedConditions.visibilityOf(registerOnline)).click();
+
+
+        ArrayList tabs2 = new ArrayList(driver.getWindowHandles());//Получение списка табов
+        driver.switchTo().window((String) tabs2.get(1));//Переключение на второй таб в браузере
+        //driver.switchTo().window(tabs2.get(0));//Переключение на первый таб в браузере
+        //driver.close();//Закрытие активного таба
+
+
+        WebElement minimal = driver.findElement(By.xpath("//*[text()='Минимальная']"));
+        wait.until(ExpectedConditions.visibilityOf(minimal)).click();
+
+
+        driver.findElement(By.xpath("//span[text()='Оформить']")).click();
+
+        fillField(By.name("insured0_surname"),"FAMILINS");
+        fillField(By.name("insured0_name"),"NAMES");
+        fillField(By.name("insured0_birthDate"),"26021990");
+
+        fillField(By.name("surname"),"Фамилинс");
+        fillField(By.name("name"),"Нэймс");
+        fillField(By.name("middlename"),"Батькович");
+        fillField(By.name("birthDate"),"26021990");
+
+        fillField(By.name("passport_series"),"5555");
+        fillField(By.name("passport_number"),"555555");
+        fillField(By.name("issueDate"),"10102010");
+        fillField(By.name("issuePlace"),"Кем то выдан");
+
+        driver.findElement(By.xpath("//*[text()='Продолжить']")).click();
+
+        WebElement errorText = driver.findElement
+                (By.xpath("//*[text()='Заполнены не все обязательные поля']"));
+
+        wait.until(ExpectedConditions.visibilityOf(errorText));
+
+        assertEquals("FAMILINS",
+                driver.findElement(By.name("insured0_surname")).getAttribute("value"));
+        assertEquals("NAMES",
+                driver.findElement(By.name("insured0_name")).getAttribute("value"));
+        assertEquals("26.02.1990",
+                driver.findElement(By.name("insured0_birthDate")).getAttribute("value"));
+
+        assertEquals("Фамилинс",
+                driver.findElement(By.name("surname")).getAttribute("value"));
+        assertEquals("Нэймс",
+                driver.findElement(By.name("name")).getAttribute("value"));
+        assertEquals("Батькович",
+                driver.findElement(By.name("middlename")).getAttribute("value"));
+        assertEquals("26.02.1990",
+                driver.findElement(By.name("birthDate")).getAttribute("value"));
+
+        assertEquals("5555",
+                driver.findElement(By.name("passport_series")).getAttribute("value"));
+        assertEquals("555555",
+                driver.findElement(By.name("passport_number")).getAttribute("value"));
+        assertEquals("10.10.2010",
+                driver.findElement(By.name("issueDate")).getAttribute("value"));
+        assertEquals("Кем то выдан",
+                driver.findElement(By.name("issuePlace")).getAttribute("value"));
+
+
+    }
+
+    public void fillField(By locator, String value){
+        driver.findElement(locator).clear();
+        driver.findElement(locator).sendKeys(value);
+
+    }
+    @After
+    public void afterTest(){
+        //driver.quit();
+
+    }
+}
